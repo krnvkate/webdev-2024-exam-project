594df73b8df9c1b06fe1c90132fb2b5d313b50ff
krnvkate <sub_kate04@mail.ru>
2024-11-29 02:20:55 +0300

    Настройка отображения моделей в админке

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Profile, FavoriteRecipe
 
-admin.site.register(Profile)
-admin.site.register(FavoriteRecipe)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user__email', 'user__first_name', 'country', 'city', 'user__is_staff')  # Показать пользовательские данные
+    search_fields = ('user__username','user__email', 'user__first_name', 'user__last_name')
+    list_filter = ('country', 'city', 'user__is_staff', 'user__is_superuser', 'user__is_active')
 
+@admin.register(FavoriteRecipe)
+class FavoriteRecipeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe', 'fav_date')  # Показать, кто лайкнул какой рецепт и когда
+    search_fields = ('user__username', 'recipe__title')
+    date_hierarchy = 'fav_date'
--- recipes/admin.py
+++ recipes/admin.py
@@ -5,16 +5,29 @@ class RecIngInline(admin.TabularInline):
     model = RecIng
     extra = 1  # Число пустых форм, которые будут отображаться для добавления ингредиентов
 
-class RecipeAdmin(admin.ModelAdmin):
-    inlines = (RecIngInline,)  # Ингредиенты как инлайн формы
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name',)  # Отображение списка ингредиентов
+    search_fields = ('name',)  # Поиск по названию ингредиента
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecIngInline]  # Отображение ингредиентов внутри формы рецепта
-    list_display = ('title', 'category', 'servings', 'cook_time', 'rating', 'status')  # Поля для отображения в списке рецептов
-admin.site.register(Category)
-admin.site.register(Step)
+    list_display = ('title', 'category', 'author', 'rating', 'status')  # Поля для отображения в списке рецептов
+    list_filter = ('status', 'category',)  # Фильтрация по статусу и категории
+    date_hierarchy = 'publish'
+    search_fields = ('title', 'author__username')  # Поиск по заголовку и автору
+    raw_id_fields = ('author',)
+
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'step_number')  # Показать рецепт и номер шага
+    ordering = ('step_number',)  # Сортировка по номеру шага
+    search_fields = ('recipe__title',)  # Поиск по названию рецепта
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category_name',)  # Поиск по полям
+    #list_filter = ('category_name',)  # Фильтрация по имени категории
